afab2fcc2b01f35f687b38a6adc814704363e794
Maulik A <maulik.ankolia2@gmail.com>
2023-10-06 17:26:11 +0100

    Update genmix_streaming_ETL_SQL.sql

--- genmix_streaming_ETL_SQL.sql
+++ genmix_streaming_ETL_SQL.sql
@@ -28,14 +28,14 @@ from stream(live.green_bronze)
 CREATE OR REFRESH STREAMING live TABLE green_gold
 AS 
 select
-  cast(ts as TIMESTAMP) ,
-  cast(biomass as DOUBLE),
-  generationmix:[1].perc  coal,
-  generationmix:[2].perc  imports,
-  generationmix:[3].perc  gas,
-  generationmix:[4].perc  nuclear,
-  generationmix:[5].perc  other,
-  generationmix:[6].perc  hydro,
-  generationmix:[7].perc  solar,
-  generationmix:[8].perc  wind
+  cast(ts as TIMESTAMP) ts ,
+  cast(biomass as DOUBLE) biomass,
+  cast(coal as DOUBLE)  coal,
+  cast(imports as DOUBLE)  imports,
+  cast(gas as DOUBLE)  gas,
+  cast(nuclear as DOUBLE)  nuclear,
+  cast(other as DOUBLE)  other,
+  cast(hydro as DOUBLE)  hydro,
+  cast(solar as DOUBLE)  solar,
+  cast(wind as DOUBLE)  wind
 from stream(live.green_silver)
